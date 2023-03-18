efc2060e6c683b8cec93b1d7c6ebe1805c0a3175
piglitch <73684837+piglitch@users.noreply.github.com>
2023-03-18 19:38:25 +0530

    new com

---
+++ insertionSort.js
@@ -0,0 +1,22 @@
+function inseSort(){
+    for(let i = 1; i < arr.length; i++){
+        let curr = arr[i];
+
+        let j = i - 1;
+
+        while(j>=0 && arr[j] > curr){
+                arr[j+1] = arr[j]; 
+                j--
+            }
+        
+        arr[j+1]  = curr;
+        
+    }
+
+
+    return arr;
+}
+
+let arr = [22,13,8,7,-12]
+
+console.log(inseSort(arr))
