bff0756039dc26480d691e098106525b64d55ffb
Avi <avibanerjee2000@gmail.com>
2023-03-31 20:27:46 +0530

    com

--- quickSort.js
+++ quickSort.js
@@ -1,14 +1,31 @@
 function qkSort(arr){
-    var pI = arr.length - 1
-    var left = []
-    var right = []
-    for (i=0, i < arr.length, i++){
-        if (arr[i]<arr){
 
-        } 
+    if(arr.length <= 1){
+        return arr;
     }
+
+    
+    const pI = arr[arr.length-1];
+    var left = [];
+    var right = [];
+
+    for (i=0; i < arr.length-1; i++){
+        if (arr[i] < pI){
+            left.push(arr[i]);
+        } 
+        else {
+            right.push(arr[i]);
+       }
+    }
+
+    sortedLeft = qkSort(left);
+    sortedRight = qkSort(right);
+    return [...sortedLeft, pI, ...sortedRight];
+    
+    
 }
 
-arr = [5, 4, 1, 0, 7, 2, 6, 3]
 
-console.log(qkSort(arr))
+arr = [1,4,5,6,2,3];
+
+console.log(qkSort(arr));
